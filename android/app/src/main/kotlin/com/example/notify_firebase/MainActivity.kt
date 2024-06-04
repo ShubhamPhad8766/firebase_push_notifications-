10ada889292d868e46635e10e0fddca0a18b9b54
Shubham D Phad <shubhamphad101@gmail.com>
2024-06-04 11:15:52 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/notify_firebase/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.notify_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: avoid_print
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:notify_firebase/firebase_options.dart';
+import 'package:notify_firebase/home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print(fcmToken);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
